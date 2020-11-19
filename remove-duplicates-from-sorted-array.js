2efca35ff5f953a4250dabf8969b90693955a0a0
Dina Ayoub <dinaayoub@hotmail.com>
2020-11-19 14:54:44 -0800

    updated remove duplicates to use the same method as remove element

--- remove-duplicates-from-sorted-array.js
+++ remove-duplicates-from-sorted-array.js
@@ -3,18 +3,13 @@
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-    for (let i = 0; i < nums.length; i++) {
+    for (let i = 1; i < nums.length; i++) {
         //get number of instances of nums[i]
-        var numberOfInstances = 0;
-        for (let j = i; j < nums.length; j++) {
-            if (nums[j] === nums[i]) numberOfInstances++;
-            else break;
-
-        }
-        //delete nums[i]
-        if (numberOfInstances > 1)
-            nums.splice(i+1, numberOfInstances - 1);
-
+        if (nums[i] === nums[i-1])
+         {
+             nums.splice(i,1);
+             i--;
+         }
     }
     return nums.length;
 };
