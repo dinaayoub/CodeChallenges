3a806bce90056b79b48102b81ef518b7c4686280
Dina Ayoub <dinaayoub@hotmail.com>
2021-02-18 17:13:28 -0800

    optimized max-subarray to be O(n)

---
+++ arrays/add-binary.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+
+var addBinary = function (a, b) {
+
+  //edge case checking
+  if (!a || a === '0') return b;
+  if (!b || b === '0') return a;
+  let aArray = a.split('');
+  let bArray = b.split('');
+  let result, n;
+  if (a.length > b.length) {
+    result = 
+  }
+  else n = b.length;
+
+  for (let i = n - 1; i >= 0; i--) {
+    if 
+  }
+
+};
--- arrays/max-subarray.js
+++ arrays/max-subarray.js
@@ -2,21 +2,38 @@
 * @param {number[]} nums
 * @return {number}
 */
+
 var maxSubArray = function (nums) {
   if (!nums) return null;
-  let maxSum = nums[0];
-  for (let i = 0; i < nums.length; i++) {
-    // console.log(`i = ${i}, and nums[i] = ${nums[i]}`);
-    if (nums[i] > 0) {
-      let currentSum = 0;
-      for (let j = i; j < nums.length; j++) {
-        // console.log(`\tj = ${j}, and nums[j] = ${nums[j]}`);
-        currentSum += nums[j];
-        if (currentSum > maxSum) maxSum = currentSum;
-      }
-    } else if (nums[i] > maxSum) maxSum = nums[i];
+  let globalSum = nums[0];
+  let localSum = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    if (localSum + nums[i] > nums[i]) localSum += nums[i];
+    else localSum = nums[i];
+
+    if (localSum > globalSum) globalSum = localSum;
   }
-  return maxSum;
+
+  return globalSum;
 };
 
-// console.log(maxSubArray([-2, -1]));
+//brute force solution O(n^2?)
+// var maxSubArray = function (nums) {
+//   if (!nums) return null;
+//   let maxSum = nums[0];
+//   for (let i = 0; i < nums.length; i++) {
+//     // console.log(`i = ${i}, and nums[i] = ${nums[i]}`);
+//     if (nums[i] > 0) {
+//       let currentSum = 0;
+//       for (let j = i; j < nums.length; j++) {
+//         // console.log(`\tj = ${j}, and nums[j] = ${nums[j]}`);
+//         currentSum += nums[j];
+//         if (currentSum > maxSum) maxSum = currentSum;
+//       }
+//     } else if (nums[i] > maxSum) maxSum = nums[i];
+//   }
+//   return maxSum;
+// };
+
+console.log(maxSubArray([-2, -1]));
