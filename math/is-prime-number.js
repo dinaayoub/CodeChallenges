349f0cbf53a121bf1c65cda18af54c19a902dc53
Dina Ayoub <dinaayoub@hotmail.com>
2021-02-20 01:18:57 -0800

    figured out prime

--- math/is-prime-number.js
+++ math/is-prime-number.js
@@ -1,37 +1,30 @@
 function isPrime(input) {
+  //Enter your code here
   let inputArray = input.split('\n');
 
   for (let j = 1; j < inputArray.length; j++) {
+    let done = false;
     let x = parseInt(inputArray[j]);
-    let half = Math.floor(x / 2);
-    // console.log(`x = ${x}, half = ${half}`);
-    if ([2, 3, 5].indexOf(x) !== -1) {
+    if (x === 2 || x === 3) {
       console.log('Prime');
-      continue;
+      done = true;
     }
-    if ((x / 2) !== half) {
-      //odd number, can be prime
-
-      //need to check whether it's divisible by any odd prime numbers smaller than its half?
-      for (let i = 3; i <= half; i = i + 2) {
+    else if (x <= 1 || x % 2 === 0 || x % 3 === 0) {
+      console.log('Not prime');
+      done = true;
+    } else {
+      for (let i = 5; i <= Math.sqrt(x); i = i + 2) {
         if (x % i === 0) {
           console.log('Not prime');
-          break;
-        }
-        if (i === half) {
-          //last iteration and x is not divisible by half, this must be a prime number. 
-          console.log('Prime');
+          done = true;
           break;
         }
       }
     }
-    else {
-      //even number, can't be prime
-      console.log('Not prime');
-    }
+    if (!done) console.log('Prime');
   }
 }
 
-isPrime('3\n12\n5\n7\n3');
-console.log('----------')
-isPrime('5\n0\n7\n1483\n10007\n10008');
+isPrime('3\n12\n5\n7\n3\n1');
+console.log('----------');
+isPrime('5\n0\n7\n1483\n10007\n10008\n625');
